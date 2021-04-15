139afd9a6711f49ce843a64ef6bb2898a8536757
AntoninB4440 <barceloantonin@gmail.com>
2021-04-15 15:38:27 +0200

    1est test on angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world';
+  title : string = 'hello-world';
+  age = "30";
+  array = ["hello there", "comment ca va ?"];
+  source = "https://img-19.ccm2.net/8vUCl8TXZfwTt7zAOkBkuDRHiT8=/1240x/smart/b829396acc244fd484c5ddcdcb2b08f3/ccmcms-commentcamarche/20494859.jpg";
+
+  handleClick() {
+    this.title = "Hello from button!"
+  }
 }
+
