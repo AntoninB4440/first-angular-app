d3769853eaf1635e0ab00cb17f1f68104aff0ea3
AntoninB4440 <barceloantonin@gmail.com>
2021-04-16 15:45:51 +0200

    adding userService + user model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserListComponent } from './user-list/user-list.component';
+import {HttpClientModule} from '@angular/common/http'
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import { UserListComponent } from './user-list/user-list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/user.ts
@@ -0,0 +1,16 @@
+export interface User {
+    index: number
+    natID: number
+    isActive: boolean
+    picture: string
+    name: string
+    company: string
+    email: string
+    phone: string
+    address: string
+    birthDate: string
+    token: string
+
+
+
+}
