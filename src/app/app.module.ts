f9953511c6b38eacf3aef5bd231f72df161d42ce
AntoninB4440 <barceloantonin@gmail.com>
2021-04-16 14:03:51 +0200

    creating header and user list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
