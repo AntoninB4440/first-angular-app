f220a283465c81002f99099739cce603e2ea7b82
AntoninB4440 <barceloantonin@gmail.com>
2021-04-20 14:34:07 +0200

    Adding new component + path for new users

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
+import { UsersAddComponent } from './users-add/users-add.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : "users" , component : UserListComponent 
+  },
+  {
+    path : "users/add" , component : UsersAddComponent 
+  },
+  {
+    path : "**" , redirectTo : "users"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserListComponent } from './user-list/user-list.component';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import { UsersAddComponent } from './users-add/users-add.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    UserListComponent
+    UserListComponent,
+    UsersAddComponent
   ],
   imports: [
     BrowserModule,
